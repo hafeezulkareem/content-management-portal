f44dd8e78b698ba1298534f1027ec9fcc6ad8377
Hafeezul Kareem Shaik <hafeezulkareem20@gmail.com>
2020-05-28 21:38:33 +0530

    Deleted CodingEditors component

--- src/CodingProblems/components/CodeEditors/CodeEditors.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { CodeEditors } from './CodeEditors'
-
-export default {
-   title: 'CodingProblems/CodeEditors'
-}
-
-export const codeEditors = () => <CodeEditors programmingLanguage='python' />
--- src/CodingProblems/components/CodeEditors/index.tsx
+++
@@ -1,1 +0,0 @@
-export { CodeEditors } from './CodeEditors'
